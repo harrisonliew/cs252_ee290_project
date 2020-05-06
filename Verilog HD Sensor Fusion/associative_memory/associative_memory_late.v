8bb78d431fa4c5a00020c51c2b2f3580afc91b32
Alisha Menon <allymenon@berkeley.edu>
2020-05-05 17:36:21 -0700

    bug fixes

--- Verilog HD Sensor Fusion/associative_memory/associative_memory_late.v
+++ Verilog HD Sensor Fusion/associative_memory/associative_memory_late.v
@@ -58,7 +58,7 @@ wire [SHIFT_CNTR_WIDTH-1:0] ShiftCntr_SN;
 // Datapath signals
 wire [0:`HV_DIMENSION-1] SimilarityOut_A_D, SimilarityOut_V_D, AM_A_class0, AM_A_class1, AM_V_class0, AM_V_class1;
 reg [`DISTANCE_WIDTH-1:0] AdderOut_A_D_P, AdderOut_V_D_P;
-wire [`DISTANCE_WIDTH-1:0] AdderOut_A_D_N, AdderOut_V_D_N;
+reg [`DISTANCE_WIDTH-1:0] AdderOut_A_D_N, AdderOut_V_D_N;
 wire CompRegisterSEN_A_S, CompRegisterSEN_V_S;
 reg OutputBuffersEN_S, ShiftMemoryEN_S, QueryHypervectorEN_S, CompRegisterEN_S, CompRegisterCLR_S, ShiftCntrEN_S, ShiftCntrCLR_S;
 wire ShiftComplete_S, popcount_complete;
@@ -158,23 +158,23 @@ integer j;
 always @(*) begin
 	for (j=0; j<`AM_CHUNK; j=j+1) begin
 		if (popcount_loop_P == 8)
-			AdderOut_A_D_N = AdderOut_A_D_P + A_chunk8[j];
+			AdderOut_A_D_N <= AdderOut_A_D_P + A_chunk8[j];
 		else if (popcount_loop_P == 7)
-			AdderOut_A_D_N = AdderOut_A_D_P + A_chunk7[j];
+			AdderOut_A_D_N <= AdderOut_A_D_P + A_chunk7[j];
 		else if (popcount_loop_P == 6)
-			AdderOut_A_D_N = AdderOut_A_D_P + A_chunk6[j];
+			AdderOut_A_D_N <= AdderOut_A_D_P + A_chunk6[j];
 		else if (popcount_loop_P == 5)
-			AdderOut_A_D_N = AdderOut_A_D_P + A_chunk5[j];
+			AdderOut_A_D_N <= AdderOut_A_D_P + A_chunk5[j];
 		else if (popcount_loop_P == 4)
-			AdderOut_A_D_N = AdderOut_A_D_P + A_chunk4[j];
+			AdderOut_A_D_N <= AdderOut_A_D_P + A_chunk4[j];
 		else if (popcount_loop_P == 3)
-			AdderOut_A_D_N = AdderOut_A_D_P + A_chunk3[j];
+			AdderOut_A_D_N <= AdderOut_A_D_P + A_chunk3[j];
 		else if (popcount_loop_P == 2)
-			AdderOut_A_D_N = AdderOut_A_D_P + A_chunk2[j];
+			AdderOut_A_D_N <= AdderOut_A_D_P + A_chunk2[j];
 		else if (popcount_loop_P == 1)
-			AdderOut_A_D_N = AdderOut_A_D_P + A_chunk1[j];
+			AdderOut_A_D_N <= AdderOut_A_D_P + A_chunk1[j];
 		else if (popcount_loop_P == 0)
-			AdderOut_A_D_N = AdderOut_A_D_P};
+			AdderOut_A_D_N <= AdderOut_A_D_P;
 	end
 end
 
@@ -182,23 +182,23 @@ integer y;
 always @(*) begin
 	for (y=0; y<`AM_CHUNK; y=y+1) begin
 		if (popcount_loop_P == 8)
-			AdderOut_V_D_N = AdderOut_V_D_P + A_chunk8[k];
+			AdderOut_V_D_N <= AdderOut_V_D_P + A_chunk8[y];
 		else if (popcount_loop_P == 7)
-			AdderOut_V_D_N = AdderOut_V_D_P + A_chunk7[k];
+			AdderOut_V_D_N <= AdderOut_V_D_P + A_chunk7[y];
 		else if (popcount_loop_P == 6)
-			AdderOut_V_D_N = AdderOut_V_D_P + A_chunk6[k];
+			AdderOut_V_D_N <= AdderOut_V_D_P + A_chunk6[y];
 		else if (popcount_loop_P == 5)
-			AdderOut_V_D_N = AdderOut_V_D_P + A_chunk5[k];
+			AdderOut_V_D_N <= AdderOut_V_D_P + A_chunk5[y];
 		else if (popcount_loop_P == 4)
-			AdderOut_V_D_N = AdderOut_V_D_P + A_chunk4[k];
+			AdderOut_V_D_N <= AdderOut_V_D_P + A_chunk4[y];
 		else if (popcount_loop_P == 3)
-			AdderOut_V_D_N = AdderOut_V_D_P + A_chunk3[k];
+			AdderOut_V_D_N <= AdderOut_V_D_P + A_chunk3[y];
 		else if (popcount_loop_P == 2)
-			AdderOut_V_D_N = AdderOut_V_D_P + A_chunk2[k];
+			AdderOut_V_D_N <= AdderOut_V_D_P + A_chunk2[y];
 		else if (popcount_loop_P == 1)
-			AdderOut_V_D_N = AdderOut_V_D_P + A_chunk1[k];
+			AdderOut_V_D_N <= AdderOut_V_D_P + A_chunk1[y];
 		else if (popcount_loop_P == 0)
-			AdderOut_V_D_N = AdderOut_V_D_P};
+			AdderOut_V_D_N <= AdderOut_V_D_P;
 	end
 end
 
