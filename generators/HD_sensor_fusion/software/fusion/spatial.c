e72598cd92cbf8d1eb2a2c74909d8a25c40d7768
Harrison Liew <harrisonliew@gmail.com>
2020-05-09 11:17:25 -0700

    zomg so close, GSR spatial works, others don't because of wraparound

--- generators/HD_sensor_fusion/software/fusion/spatial.c
+++ generators/HD_sensor_fusion/software/fusion/spatial.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+#include "util.h"
 #include "fusion_funcs.h"
 #include "init.h"
 //the data.h and mems_<early/late>.h file can be created directly in MATLAB (after the simulation)
@@ -11,20 +12,29 @@
 
 int main(){
  
-    float buffer[channels_GSR];
-	uint64_t q[bit_dim + 1];
+    float buffer[channels_ECG];
+	uint64_t q_cpu[bit_dim + 1], q_hwacha[bit_dim+1];
           
-	//Spatial Encoder
+	//CPU Spatial Encoder
     #if PROFILE == 1
         uint64_t spatial_start = read_cycles();
+
+    memcpy(buffer, TEST_SET_ECG[0], sizeof(TEST_SET_ECG[0]));
+    computeNgram(channels_ECG, cntr_bits_ECG, buffer, iM_ECG, projM_pos_ECG, projM_neg_ECG, q_cpu);
+
+        printf("CPU spatial cycles: %llu\n", read_cycles() - spatial_start);
+        spatial_start = read_cycles();
     #endif
 
-    //spatially encode first sample
-    memcpy(buffer, TEST_SET_GSR[0], sizeof(TEST_SET_GSR[0]));
-    computeNgram(channels_GSR, cntr_bits_GSR, buffer, iM_GSR, projM_pos_GSR, projM_neg_GSR, q);
+	//Hwacha Spatial Encoder
+    vec_computeNgram(channels_ECG, cntr_bits_ECG, buffer, iM_ECG, projM_pos_ECG, projM_neg_ECG, q_hwacha);
 
     #if PROFILE == 1
-        printf("Spatial cycles: %llu\n", read_cycles() - spatial_start);
+        printf("Hwacha spatial cycles: %llu\n" , read_cycles() - spatial_start);
+        for(int i = 0; i < bit_dim+1; i++) {
+            printf("cpu: %llx   hwacha: %llx\n", q_cpu[i], q_hwacha[i]);
+        }
+        printf("Verify: %d\n", verifyuint64_t(bit_dim+1, q_cpu, q_hwacha));
     #endif
 
     return 0; 
--- generators/HD_sensor_fusion/software/fusion/vec_computeNgram.c
+++ generators/HD_sensor_fusion/software/fusion/vec_computeNgram.c
@@ -8,61 +8,15 @@
 
 void vec_computeNgram(int channels, int cntr_bits, float buffer[], uint64_t iM[][bit_dim + 1], uint64_t projM_pos[][bit_dim + 1], uint64_t projM_neg[][bit_dim + 1], uint64_t query[bit_dim + 1]){
 
+    #if PROFILE==1
+        uint64_t start = read_cycles();
+    #endif
+
     uint64_t chHV, chHV2;
 
     int cntr_init = (1 << (cntr_bits-1)) - (channels+1)/2 - 1;
     uint64_t cntr[cntr_bits];
     uint64_t temp, carry;
-
-    #if PROFILE == 1
-    uint64_t start = read_cycles();
-
-	//Spatial Encoder: captures the spatial information for a given time-aligned samples of channels
-	for(int i = 0; i < bit_dim + 1; i++){
-
-        //fastest componentwise majority: bit serial, word parallel
-        //initialize counter to 2^(cntr_bits)/2 - channels/2
-        //that way, the counter msb uint becomes the final query HV
-
-        for(int n = 0; n < cntr_bits; n++) {
-            if ((cntr_init  & (1 << n)) == 0) {
-                cntr[n] = 0;
-            } else {
-                cntr[n] = 0xFFFFFFFFFFFFFFFFULL;
-            }
-        }
-
-        for(int j = 0; j < channels+1; j++) {
-
-            // calc chHV
-            if(j == channels) {
-                chHV ^= chHV2;
-            } else {
-                // slight hit if we don't check against 0 exactly also
-                //chHV = buffer[j] == 0.0 ? iM[j][i] : (iM[j][i] ^ (buffer[j] > 0.0 ? projM_pos[j][i] : projM_neg[j][i]));
-                chHV = iM[j][i] ^ (buffer[j] >= 0.0 ? projM_pos[j][i] : projM_neg[j][i]);
-            }
-            if(j == 1) chHV2 = chHV;
-
-            // incremental popcount
-            carry = cntr[0] & chHV;
-            cntr[0] ^= chHV; 
-            for(int n = 1; n < cntr_bits; n++) {
-                temp = cntr[n];
-                cntr[n] ^= carry;
-                carry &= temp;
-            }
-        }
-        
-        query[i] = cntr[cntr_bits-1];
-
-	}
-
-    printf("cpu spatial encoding cycles: %llu\n", read_cycles() - start);
-    start = read_cycles();
-    #endif
-
-    //Hwacha implementation
 
     // data registers:
     // vv0: iM
@@ -77,7 +31,7 @@ void vec_computeNgram(int channels, int cntr_bits, float buffer[], uint64_t iM[]
     // va1: &projM_pos
     // va2: &projM_neg
     // va3: query
-    asm volatile ("vsetcfg %0" : : "r" (VCFG(cntr_bits + 6, 0, 2, 1)));
+    asm volatile ("vsetcfg %0" : : "r" (VCFG(cntr_bits + 6, 0, 0, 2)));
 
     // for counter
     uint64_t one = 0x1ULL;
@@ -99,11 +53,9 @@ void vec_computeNgram(int channels, int cntr_bits, float buffer[], uint64_t iM[]
 
     for(int i = 0; i < bit_dim + 1; ) {
 
-        printf("i: %d\n", i);
-
         // loop setup
         int consumed;
-        asm volatile ("vsetvl %0, %1" : "=r" (consumed) : "r" (i));
+        asm volatile ("vsetvl %0, %1" : "=r" (consumed) : "r" (bit_dim+1-i));
 
         // init cntr
         switch(cntr_bits) {
@@ -115,7 +67,7 @@ void vec_computeNgram(int channels, int cntr_bits, float buffer[], uint64_t iM[]
             case 7:
                 // call 7b cntr_init vf block
                 asm volatile ("la %0, cntr_init_7b_v" : "=r" (cntr_init_7b_addr));
-                asm volatile ("vf 0(%0)" : : "r" (cntr_init_7b_addr));
+                //asm volatile ("vf 0(%0)" : : "r" (cntr_init_7b_addr));
                 break;
             default:
                 printf("only 6 or 7 counter bits supported!");
@@ -186,6 +138,6 @@ void vec_computeNgram(int channels, int cntr_bits, float buffer[], uint64_t iM[]
     asm volatile ("fence");
 
     #if PROFILE==1
-    printf("hwacha spatial encoding cycles: %llu\n", read_cycles() - start);
+        printf("spatial encoding cycles: %llu\n", read_cycles() - start);
     #endif
 }
